1ac6693309b6a4c823cce5c99406df8a5dba80bb
wangwuy2003 <nquanghuy.2003@gmail.com>
2023-03-10 20:18:37 +0700

    reinitialized files

---
+++ QuanLyQuanCaPhe/fLogin.cs
@@ -0,0 +1,25 @@
+namespace QuanLyQuanCaPhe
+{
+    public partial class fLogin : Form
+    {
+        public fLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
